b9ae3622eb375be19eebbab93ee4eac7e9421b68
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-02-06 17:37:20 +0530

    commit

--- Hiru_test/lambda.js
+++ Hiru_test/lambda.js
@@ -1,6 +1,18 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
+let SL = require('@slappforge/slappforge-sdk');
+const sqs = new SL.AWS.SQS(AWS);
+exports.handler = function (event, context, callback) {
+	sqs.sendMessage({
+		MessageBody: 'This is a test sample message',
+		QueueUrl: 'https://sqs.us-east-2.amazonaws.com/480964559519/new_as2',
+		DelaySeconds: '0',
+		MessageAttributes: {}
+	}, function (data) {
+		// your logic (logging etc) to handle successful message delivery, should be here
+	}, function (error) {
+		// your logic (logging etc) to handle failures, should be here
+	});
 
 
-	callback(null,'Successfully executed');
+	callback(null, 'Successfully executed');
 }

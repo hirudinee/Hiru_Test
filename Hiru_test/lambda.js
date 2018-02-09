437b3374392850216e51d3e5cc08680d4006e6fa
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-02-09 15:05:03 +0530

    test

--- Hiru_test/lambda.js
+++ Hiru_test/lambda.js
@@ -1,18 +1,19 @@
 let AWS = require('aws-sdk');
-let SL = require('@slappforge/slappforge-sdk');
-const sqs = new SL.AWS.SQS(AWS);
+const kinesis = new AWS.Kinesis();
 exports.handler = function (event, context, callback) {
-	sqs.sendMessage({
-		MessageBody: 'This is a test sample message',
-		QueueUrl: 'https://sqs.us-east-2.amazonaws.com/480964559519/new_as2',
-		DelaySeconds: '0',
-		MessageAttributes: {}
-	}, function (data) {
-		// your logic (logging etc) to handle successful message delivery, should be here
-	}, function (error) {
-		// your logic (logging etc) to handle failures, should be here
-	});
 
+
+	kinesis.describeStream({
+		StreamName: 'Hiru'
+	}).promise()
+		.then(data => {
+			// your logic goes here
+
+		})
+		.catch(err => {
+			// error handling goes here
+			
+		});
 
 	callback(null, 'Successfully executed');
 }
